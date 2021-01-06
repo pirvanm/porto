6a690533e0d464f1f46a1dff6c889f133f19cbdd
Pirvan Marian <pirvan.marian@gmail.com>
2021-01-07 00:40:41 +0200

    modificari pentru postarile blog

--- s9.php
+++ s9.php
@@ -7,14 +7,35 @@
 
 
     <div class="col-md-4">
-        America
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="postare_1.png" alt="Card image cap">
+            <div class="card-body">
+                <!-- <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a> -->
+            </div>
+        </div>
     </div>
 
     <div class="col-md-4">
-        America
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="postare_2.png" alt="Card image cap">
+            <div class="card-body">
+                <!-- <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a> -->
+            </div>
+        </div>
     </div>
 
     <div class="col-md-4">
-        America
+        <div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="postare_3.png" alt="Card image cap">
+            <div class="card-body">
+                <!-- <h5 class="card-title">Card title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a> -->
+            </div>
+        </div>
     </div>
 </div>
