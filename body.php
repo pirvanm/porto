366a18fa5ab66dbe0b765f04ff7183541f9dd38d
Pirvan Marian <pirvan.marian@gmail.com>
2021-01-06 23:22:34 +0200

    sectiounea 6

--- body.php
+++ body.php
@@ -17,6 +17,7 @@
         // include('s3.php');
         //include('s4.php');
         include('s5.php');
+        include('');
         ?>
 
 
---
+++ s6.php
@@ -0,0 +1,5 @@
+<div id="6" class="row">
+    <div class="col-md-4">
+        <img src="">
+    </div>
+</div>
