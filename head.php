42c63171ba717a194ee7881c25d0accac7c1a07f
Pirvan Marian <pirvan.marian@gmail.com>
2021-01-15 21:28:29 +0200

    stilizare fonturi pentru css

--- head.php
+++ head.php
@@ -7,7 +7,8 @@
     <link rel="stylesheet" media="screen and (max-width-device: 576px)" href="css/mobile.css">
     <link rel="stylesheet" media="screen and (min-width: 1200px)" href="css/large.css">
     <!-- <link rel="stylesheet" media="screen and (max-width: 600px)" href="smallscreen.css"> -->
-
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap" rel="stylesheet">
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
